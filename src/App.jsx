55598ef795d23126179ac329593bace6070b82e5
Charan <acharan1526@gmail.com>
2026-06-11 16:33:14 +0530

    Fix component bugs and update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import './App.css'
-
+import Header from './Header'
+import Products from './Products.jsx'
 function App() {
 
   return (
     <>
+      <Header />
       <section style={{ textAlign: "center" }}>
-        <header style={{ display: "flex", justifyContent: "space-evenly" }}>
-          <img height={30} width={100} src="https://zerodha.com/static/images/logo.svg" alt="logo" />
-          <nav style={{ display: "flex", gap: "20px", alignItems: "center" }}>
-            <a href="">Signup</a>
-            <a href="">About</a>
-            <a href="">Products</a>
-            <a href="">Pricing</a>
-            <a href="">Support</a>
-          </nav>
-        </header>
         <img src="https://zerodha.com/static/images/landing.svg" alt="home-logo" />
         <h2>Invest in everything</h2>
         <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
         <button id="btn">SignUp</button>
       </section>
+      <Products />
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <header style={{ display: "flex", justifyContent: "space-evenly" }}>
+                <img height={30} width={100} src="https://zerodha.com/static/images/logo.svg" alt="logo" />
+                <nav style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+                    <a href="">Signup</a>
+                    <a href="">About</a>
+                    <a href="">Products</a>
+                    <a href="">Pricing</a>
+                    <a href="">Support</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
