938509034896abe31075432d0eb41f8f6b042db1
Charan <acharan1526@gmail.com>
2026-06-16 11:51:17 +0530

    Remove BACKENDECOM from this repo to move to separate backend repo

--- BACKENDECOM/routes/Authroute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-let router = express.Router()
-const { register, login } = require('../controllers/authcontroller')
-router.post('/register', register)
-router.post('/login', login)
-module.exports = router
